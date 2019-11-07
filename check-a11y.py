cffc365e2919e1b0201f60f4b502048330674f62
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-11-07 11:30:39 +0000

    Updated the check-a11y.py script to run pa11y against all index.html files in the source directory (/srv)

--- check-a11y.py
+++ check-a11y.py
@@ -2,8 +2,20 @@
 import os
 import glob
 import platform
-print(platform.python_version())
-print("Working")
-os.chdir("/srv")
-for file in glob.glob("index.html"):
-    print(file)
+import subprocess
+
+print("Checking /srv for a11y issues...")
+
+def get_all_files_with_ext(directory, extension):
+    files_list = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(extension):
+                files_list.append(os.path.join(root, file))
+    return files_list
+
+
+if __name__ == "__main__":
+    html_files = get_all_files_with_ext("/srv", ".html")
+    for html_file in html_files:
+        subprocess.call(["pa11y", "--config", "/tmp/pa11y.config.json", html_file])
